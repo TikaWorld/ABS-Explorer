465ecbc2db47cbd2f8938aa62d1abf898098e578
TikaWorld <tikaworld0416@gmail.com>
2019-04-22 23:36:40 +0900

    Set Ignore, Add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# ABS Explorer
+
+### Translated Amazon Best Seller Explorer
+Find Best Seller in Amazon and find translated books
+
---
+++ src/test/java/com/toyproject/abs_explorer/AbsExplorerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.toyproject.abs_explorer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AbsExplorerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
