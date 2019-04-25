51f6b6b84f9646fb630eb93cff184ebe2fa84449
TikaWorld <tikaworld0416@gmail.com>
2019-04-25 17:24:22 +0900

    Optimize KyoboSearcch Method

--- src/main/java/com/toyproject/abs_explorer/KyoboSearcher.java
+++ src/main/java/com/toyproject/abs_explorer/KyoboSearcher.java
@@ -10,26 +10,8 @@ public class KyoboSearcher {
         dataCrawler = new DataCrawler(this.url);
     }
 
-    public void search(String word) {
-        Document doc = dataCrawler.parsehttps("?vPstrKeyWord=" + regularization(word) + "&vPplace=top");
-    }
-
-    protected String regularization(String sentence) {
-        String words[] = sentence.split(" ");
-        String newStence = "";
-        for(String word: words) {
-            newStence = newStence + word;
-            newStence = newStence + "%20";
-        }
-        return replaceLast(newStence, "%20", "");
-    }
-
-    private static String replaceLast(String string, String toReplace, String replacement) {
-        int pos = string.lastIndexOf(toReplace);
-        if (pos > -1) {
-            return string.substring(0, pos)+ replacement + string.substring(pos + toReplace.length(), string.length());
-        } else {
-            return string;
-        }
+    public Document search(String word) {
+        Document doc = dataCrawler.parsehttps("?vPstrKeyWord=" + word.replaceAll(" ", "%20") + "&vPplace=top");
+        return doc;
     }
 }
--- src/test/java/com/toyproject/abs_explorer/KyoboSearcherTest.java
+++ src/test/java/com/toyproject/abs_explorer/KyoboSearcherTest.java
@@ -5,10 +5,8 @@ import org.junit.Test;
 
 public class KyoboSearcherTest {
     @Test
-    public void regularizationTest() {
+    public void searchTest() {
         KyoboSearcher kyoboSearcher = new KyoboSearcher();
-        String regularedWord =kyoboSearcher.regularization("Computer Architecture");
-        System.out.println(regularedWord);
-        assertEquals("Computer%20Architecture", regularedWord);
+        System.out.println(kyoboSearcher.search("Computer Architecture"));
     }
 }
