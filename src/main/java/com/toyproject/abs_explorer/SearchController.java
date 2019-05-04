eb310b62d20dc5b599b50057e3c6df899447fe6f
TikaWorld <tikaworld0416@gmail.com>
2019-05-04 21:04:39 +0900

    Add DAO(MySQL)

--- src/main/java/com/toyproject/abs_explorer/SearchController.java
+++ src/main/java/com/toyproject/abs_explorer/SearchController.java
@@ -1,5 +1,13 @@
 package com.toyproject.abs_explorer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SearchController {
+
+    @Autowired
+    private RankDao rankDao;
+
 
 }
